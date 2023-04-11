48a4090c51cda7dc9f74e4c4413488e6dc2532f9
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-10 21:06:44 -0600

    Added url as prop in SingleCard

--- src/components/apps/Apps.js
+++ src/components/apps/Apps.js
@@ -3,14 +3,20 @@ import React from 'react';
 // React Bootstrap
 import { Container, Row, Card } from 'react-bootstrap';
 
+// Packages
+import PropTypes from 'prop-types';
+
 // Components
 import Header from '../main/Header';
 
 const SingleApp = (props) => {
-	const { img, title, desc } = props;
+	const { img, title, desc, url } = props;
 
 	return (
-		<Card className='app-card shadow-lg p-0 mb-5 bg-body-tertiary rounded text-center'>
+		<Card
+			className='app-card shadow-lg p-0 mb-5 bg-body-tertiary rounded text-center'
+			onClick={() => window.location.href = url}
+		>
 			<Card.Img className='app-card-img' src={img} />
 			<Card.Body className='app-card-body'>
 				<Card.Title as={'h2'}>{title}</Card.Title>
@@ -18,6 +24,13 @@ const SingleApp = (props) => {
 			</Card.Body>
 		</Card>
 	);
+}
+
+SingleApp.prototypes = {
+	img: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	desc: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired
 }
 
 const Apps = () => {
@@ -33,11 +46,13 @@ const Apps = () => {
 						img={process.env.PUBLIC_URL + '/assets/setup.png'}
 						title='Admin'
 						desc='Allows you to configure the services to be monitored.'
+						url={process.env.REACT_APP_ADMIN}
 					/>
 					<SingleApp
 						img={process.env.PUBLIC_URL + '/assets/server-status.png'}
 						title='Overview'
 						desc='Allows you to monitor services.'
+						url={process.env.REACT_APP_OVERVIEW}
 					/>
 				</Row>
 			</Container>
